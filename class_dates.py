120fb719c54a7059fac2c248e87dab7c73991711
Martin Skarzynski <marskar@gmail.com>
2018-08-16 12:21:43 -0400

    add script to make folders for each class session

---
+++ class_dates.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+from typing import List
+from pprint import pprint
+from os import makedirs
+
+def class_dates(start: str, end: str) -> List[str]:
+    def format_date(date_str):
+        return datetime.strptime(date_str, "%Y-%m-%d").date()
+    start_date, end_date = map(format_date, [start, end])
+    weeks_int = int((end_date - start_date).days / 7)
+    return [(start_date + timedelta(weeks=i)).strftime("%Y-%m-%d")
+            for i in range(weeks_int+1)]
+
+cd = class_dates(start = "2018-09-13", end = "2018-12-13")
+pprint(cd)
+any(map(makedirs, cd))
