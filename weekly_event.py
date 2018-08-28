2a492c12a0f1b912ab90927521cb456e50b8845b
Martin Skarzynski <marskar@gmail.com>
2018-08-28 15:30:55 -0400

    weekly_event.py takes datetime objects as inputs

--- weekly_event.py
+++ weekly_event.py
@@ -30,9 +30,12 @@ def weekly_event(start: Union[int, str],
         `>>> weekly_event(start = 20180913, end = 20180917, weekdays = 1234560)`
         ['2018-09-13', '2018-09-14', '2018-09-15', '2018-09-16', '2018-09-17']
         All arguments that do not have default values can be strings.
-        `>>> weekly_event('20180913', end = '20180917', weekdays = '03456')`
+        `>>> weekly_event('20180913', '20180917', '03456')`
         Punctuations and whitespace in non-default string arguments are ignored.
-        `>>> weekly_event('2018-09-13', '2018-09-17', weekdays = '0, 3, 4, 5')`
+        `>>> weekly_event('2018-09-13', '2018-09-17','0, 3, 4, 5')`
+        The start and end arguments can be datetime objects.
+        `>>> from datetime import date`
+        `>>> weekly_event(date.today(), date.today().replace(year=2019), 12345)`
         The weekdays argument can be any iterable, e.g. list or range object.
         `>>> weekly_event(20180913, 20180917, weekdays = [0, 1, 2, 3, 4, 5, 6])`
         `>>> weekly_event(20180913, 20180917, weekdays = range(0, 7))`
@@ -45,12 +48,13 @@ def weekly_event(start: Union[int, str],
     def parse_date(date_str):
         return datetime.strptime(keep_digits(date_str), input_format).date()
 
-    start_date, end_date = map(parse_date, [str(start), str(end)])
-    day_range = range(int((end_date - start_date).days) + 1)
+    if any(isinstance(x, (str, int)) for x in (start, end)):
+        start, end = map(parse_date, [str(start), str(end)])
+    day_range = range(int((end - start).days) + 1)
 
     if isinstance(weekdays, (str, int)):
         weekdays = [int(x) for x in list(keep_digits(str(weekdays)))]
 
-    return [(start_date + timedelta(days=i)).strftime(output_format)
+    return [(start + timedelta(days=i)).strftime(output_format)
             for i in day_range
-            if (start_date + timedelta(days=i)).weekday() in weekdays]
+            if (start + timedelta(days=i)).weekday() in weekdays]
