991fa7ff15125625437466501a539eb74ee4e80c
Martin Skarzynski <marskar@gmail.com>
2018-09-09 07:39:06 -0400

    absolute imports are working

--- packages/nbless/nbtest.py
+++ packages/nbless/nbtest.py
@@ -1,2 +1,6 @@
 from packages.nbless.nbuild import nbuild
-nbuild(["packages/nbless/README.md", "packages/nbless/plot.py", "packages/nbless/notes.txt"])
+from packages.nbless.nbexec import nbexec
+from packages.nbless.nbless import nbless
+nbuild(["packages/nbless/README.md", "packages/nbless/plot.py", "packages/nbless/notes.txt"], output_path="packages/nbless/notebooks")
+nbexec("packages/nbless/notebooks/raw.ipynb", output_path="packages/nbless/notebooks")
+nbless(["packages/nbless/README.md", "packages/nbless/plot.py", "packages/nbless/notes.txt"], nbexec_path="packages/nbless/notebooks")
