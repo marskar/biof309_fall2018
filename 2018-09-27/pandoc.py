f48960574a0d6aee1916718da0f6acdd0135b638
Martin Skarzynski <marskar@gmail.com>
2018-09-27 16:00:26 -0400

    add css files to pandoc HTML slide examples

--- 2018-09-27/pandoc.py
+++ 2018-09-27/pandoc.py
@@ -13,17 +13,22 @@ write_file('2018-09-27/about.rst', output)
 # You can make a slidy HTML slide deck
 slides = pypandoc.convert_file('2018-09-27/slides.md', to='slidy', extra_args=['-s'])
 
-# You can make a slidy HTML slide deck
+# And specify style with a css file, e.g. https://github.com/alblaine/countess
 slides = pypandoc.convert_file('2018-09-27/slides.md', to='slidy', extra_args=['-s', '--css=style.css'])
 
-# You can make a dzslides HTML slide deck
+# Another option is to make a dzslides HTML slide deck
 slides = pypandoc.convert_file('2018-09-27/slides.md', to='dzslides', extra_args=['-s'])
+
+# And now with a css style: https://gist.github.com/bjin/2264634
+slides = pypandoc.convert_file('2018-09-27/slides.md', to='dzslides', extra_args=['-s', '--css=slide.css'])
 
 # You can also make a revealjs HTML slide deck
 slides = pypandoc.convert_file('2018-09-27/slides.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js'])
 
-# Try a different revealjs theme
+# Try the sky revealjs theme
 slides = pypandoc.convert_file('2018-09-27/slides.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js', '-V', 'theme=sky'])
 
-write_file('2018-09-27/about.html', slides)
+# Try the moon revealjs theme
+slides = pypandoc.convert_file('2018-09-27/slides.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js', '-V', 'theme=moon'])
 
+write_file('2018-09-27/about.html', slides)
